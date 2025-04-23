a11e1df74f594506384257822cab44bf2ea504e7
igrsilveira <igrsilveira.io@gmail.com>
2025-04-23 16:14:33 -0300

    User update scenario added

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,47 @@
+import userData from '../fixtures/user-data.json'
+describe('template spec', () => {
+
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: ".--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input",
+    lastNameField: ":nth-child(3) > :nth-child(2) > .oxd-input",
+    genericField: ".oxd-input--active",
+    NationalityField: ".oxd-select-text--active",
+    saveButton: ".orangehrm-card-container > .oxd-form > .oxd-form-actions > .oxd-button",
+
+  }
+
+  it.only('User Info Update - Success', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FirstnameTest')
+    cy.get(selectorsList.lastNameField).clear().type('LastnameTest')
+    cy.get(selectorsList.genericField).eq(3).clear().type('EmploTest')
+    cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(5).clear().type('DriversLicenseTest')
+    cy.get(selectorsList.genericField).eq(6).clear().click().type('2025-03-03');
+    cy.get(selectorsList.saveButton).click()
+    cy.get('.oxd-toast-content').should('contain', 'Successfully Updated')
+    // cy.get(selectorsList.NationalityField).click().type('Brazilian');    // cy.get(selectorsList.genericField).eq(7).type('DriversLicenseNumber')
+  })
+
+//   it('Login - Fail', () => {
+//     cy.visit('/auth/login')
+//     cy.get(selectorsList.usernameField).type(userData.userFail.username)
+//     cy.get(selectorsList.passwordField).type(userData.userFail.password)
+//     cy.get(selectorsList.loginButton).type('Test')
+//     cy.get(selectorsList.wrongCredentialAlert).click()
+//   })
+// })
+})
