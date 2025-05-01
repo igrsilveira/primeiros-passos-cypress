f6fd91c6e994c740a4d4d05f3104275a426a99ba
igrsilveira <igrsilveira.io@gmail.com>
2025-05-01 16:42:37 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,14 +1,15 @@
 import userData from '../fixtures/user-data.json'
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js' 
+import MenuPage from '../pages/menuPage.js'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
+
 describe('template spec', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']",
-    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
     firstNameField: ".--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input",
     lastNameField: ":nth-child(3) > :nth-child(2) > .oxd-input",
     genericField: ".oxd-input--active",
@@ -20,20 +21,19 @@ describe('template spec', () => {
   }
 
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
+
+    dashboardPage.checkDashboardPage()
+
+    menuPage.accessMyInfo()
+
     cy.get(selectorsList.firstNameField).clear().type('FirstnameTest')
     cy.get(selectorsList.lastNameField).clear().type('LastnameTest')
     cy.get(selectorsList.genericField).eq(3).clear().type('EmploTest')
     cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
     cy.get(selectorsList.genericField).eq(5).clear().type('DriversLicenseTest')
     cy.get(selectorsList.genericField).eq(6).clear().click().type('2025-03-03');
-    
     cy.get(selectorsList.genericCombobox).eq(0).click();
     cy.get(selectorsList.secondItemCombobox).click(); //Nacionalidade
     cy.get(selectorsList.genericCombobox).eq(1).click();
